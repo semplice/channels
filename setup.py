765956e84b48cd431c98cc3d60db9eabc894b3e7
Eugenio Paolantonio (g7) <me@medesimo.eu>
2015-12-16 19:32:53 +0100

    0.70.1-1 (devel)
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from distutils.core import setup
 
 setup(
 	name='channels',
-	version='0.70.0',
+	version='0.70.1',
 	description='Update channels management front-end',
 	author='Eugenio Paolantonio',
 	author_email='me@medesimo.eu',
